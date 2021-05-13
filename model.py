dce78a460d0daa3fa259f8e48710c984bd44cd5d
seah78 <githubseah.pro>
2021-05-13 20:09:59 +0200

    Créez votre première Zone

--- model.py
+++ model.py
@@ -23,12 +23,37 @@ class Position:
     def latitude(self):
         return self.latitude_degrees * math.pi / 180       
 
+class Zone:
+
+    ZONES = []
+    MIN_LONGITUDE_DEGREES = -180
+    MAX_LONGITUDE_DEGREES = 180
+    MIN_LATITUDE_DEGREES = -90
+    MAX_LATITUDE_DEGREES = 90
+    WIDTH_DEGREES = 1
+    HEIGHT_DEGREES = 1
+
+    def __init__(self, corner1, corner2):
+        self.corner1 = corner1
+        self.corner2 = corner2
+        self.inhabitants = 0
+
+    @classmethod
+    def initialize_zones(cls):
+        for latitude in range (cls.MIN_LATITUDE_DEGREES, cls.MAX_LATITUDE_DEGREES, cls.HEIGHT_DEGREES):
+            for longitude in range (cls.MIN_LONGITUDE_DEGREES, cls.MAX_LONGITUDE_DEGREES, cls.WIDTH_DEGREES):
+                bottom_left_corner = Position(longitude, 1)
+                top_right_corner = Position(longitude + cls.WIDTH_DEGREES, latitude + cls.HEIGHT_DEGREES)
+                zone = Zone(bottom_left_corner, top_right_corner)
+                cls.ZONES.append(Zone)
+            print(len(cls.ZONES))    
+
 def main():
     for agent_attributes in json.load(open("agents-100k.json")):
         latitude=agent_attributes.pop("latitude")
         longitude=agent_attributes.pop("longitude")
         position=Position(longitude, latitude)
         agent = Agent(position, **agent_attributes)
-        print(agent.position.latitude)
+        Zone.initialize_zones()
 
 main()
