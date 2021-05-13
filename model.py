bd3482cffe3f4f1c5d6b79367230839fc0b575c2
seah78 <github@seah.pro>
2021-05-13 17:23:35 +0200

    Créez la classe Position

--- model.py
+++ model.py
@@ -1,11 +1,34 @@
+import json
+import math
+
 class Agent:
-	def __init__(self, agreeableness):
-		self.agreeableness = agreeableness
+    def __init__(self, position, **agent_attributes):
+        self.position=position
+        for attr_name, attr_value in agent_attributes.items():
+            setattr(self, attr_name, attr_value)
 
-	def say_hello(self, first_name):
-		return "Bien le bonjour " + first_name + "!"
+    def say_hello(self, first_name):
+        return "Bien le bonjour " + first_name+ "!"
 
+class Position:
+    def __init__(self, longitude_degrees, latitude_degrees):
+        self.longitude_degrees=longitude_degrees
+        self.latitude_degrees=latitude_degrees
 
-agent = Agent(0)
-print(agent.agreeableness)
+    @property    
+    def longitude(self):
+        return self.longitude_degrees * math.pi / 180
 
+    @property    
+    def latitude(self):
+        return self.latitude_degrees * math.pi / 180       
+
+def main():
+    for agent_attributes in json.load(open("agents-100k.json")):
+        latitude=agent_attributes.pop("latitude")
+        longitude=agent_attributes.pop("longitude")
+        position=Position(longitude, latitude)
+        agent = Agent(position, **agent_attributes)
+        print(agent.position.latitude)
+
+main()
